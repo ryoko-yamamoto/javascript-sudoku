f515c8ac8db97972962e40b58f7d8c6ef2b526d7
ryoko-yamamoto <sj.rdragon.2min@gmail.com>
2024-07-01 00:48:20 -0700

    add comments

--- sudoku_random.js
+++ sudoku_random.js
@@ -2,14 +2,13 @@ let numSelected = null;
 let tileSelected = null;
 let timerStarted = false; //true: start, false:not start
 let timerInterval;
-let timeRemaining = 15 * 60; // 15 minutes in seconds
-
+let timeRemaining = 15 * 60; // 15 minutes
 let errors = 0;
 const maxErrors = 15; // Maximum number of allowed errors
-let difficulty = 50;
+let difficulty = 50; //midium
 
-//initialized the number of board
-let board = [
+const firstArr = [
+  //initialize the board and answer
   ["-", "-", "-", "-", "-", "-", "-", "-", "-"],
   ["-", "-", "-", "-", "-", "-", "-", "-", "-"],
   ["-", "-", "-", "-", "-", "-", "-", "-", "-"],
@@ -20,34 +19,23 @@ let board = [
   ["-", "-", "-", "-", "-", "-", "-", "-", "-"],
   ["-", "-", "-", "-", "-", "-", "-", "-", "-"],
 ];
+board = firstArr;
+solution = firstArr;
 
-//initialized the number of board
-let solution = [
-  ["-", "-", "-", "-", "-", "-", "-", "-", "-"],
-  ["-", "-", "-", "-", "-", "-", "-", "-", "-"],
-  ["-", "-", "-", "-", "-", "-", "-", "-", "-"],
-  ["-", "-", "-", "-", "-", "-", "-", "-", "-"],
-  ["-", "-", "-", "-", "-", "-", "-", "-", "-"],
-  ["-", "-", "-", "-", "-", "-", "-", "-", "-"],
-  ["-", "-", "-", "-", "-", "-", "-", "-", "-"],
-  ["-", "-", "-", "-", "-", "-", "-", "-", "-"],
-  ["-", "-", "-", "-", "-", "-", "-", "-", "-"],
-];
-
-function shuffle(array) {
+const shuffle = (array) => {
   //Fisher-Yates shuffle algorithm
   for (let i = array.length - 1; i > 0; i--) {
-    //row 0-8
-    const j = Math.floor(Math.random() * (i + 1)); //column 0-8
+    const j = Math.floor(Math.random() * (i + 1));
+    // floor: round down
+    // random: make random number between 0 to 1
     [array[i], array[j]] = [array[j], array[i]];
   }
   return array;
-}
+};
 
-function isValid(board, row, col, num) {
+const isValid = (board, row, col, num) => {
   //validate we can put the number on the cell
   for (let x = 0; x < 9; x++) {
-    // row
     if (board[row][x] == num) return false; //cannot put the number
   }
   for (let x = 0; x < 9; x++) {
@@ -62,69 +50,70 @@ function isValid(board, row, col, num) {
     }
   }
   return true; //we can put the number
-}
+};
 
-function fillBoard(board) {
+const fillBoard = (board) => {
   let numbers = shuffle([1, 2, 3, 4, 5, 6, 7, 8, 9]);
   for (let i = 0; i < 9; i++) {
-    //row
     for (let j = 0; j < 9; j++) {
-      //column
       if (board[i][j] == "-") {
         // check if the cell is empty
         for (let num of numbers) {
           if (isValid(board, i, j, num)) {
+            //i:row, j:col
             board[i][j] = num; //put the number
-            if (fillBoard(board)) return true;
-            board[i][j] = "-"; //if we cannot put the number, we need turn the tile to empty
+            if (fillBoard(board)) return true; //Recursively try to fill the rest of the board
+            board[i][j] = "-"; //if we cannot put the number, we need turn the cell to empty
           }
         }
-        return false;
+        return false; // If no number can be placed in the cell, backtrack
       }
     }
   }
-  return true;
-}
+  return true; //All cells are successfully filled
+};
 
-function removeDigits(board, numToRemove) {
+const removeDigits = (board, numToRemove) => {
   while (numToRemove > 0) {
     //loop until the number of  remove turns zero
     let i = Math.floor(Math.random() * 9); //row 0-8
     let j = Math.floor(Math.random() * 9); //column 0-8
-    //choose the tile randomly
+    //choose the cell randomly
     if (board[i][j] != "-") {
       //if the cell is not empty
       board[i][j] = "-"; //make the cell empty
       numToRemove--; //reduce the number of remove
     }
   }
-}
+};
 
-function checkWin() {
+const checkWin = () => {
   for (let r = 0; r < 9; r++) {
+    //row 0-8
     for (let c = 0; c < 9; c++) {
+      //col 0-8
       if (board[r][c] === "-" || board[r][c] != solution[r][c]) {
-        return false;
+        return false; //lose
       }
     }
   }
-  return true;
-}
+  return true; //win
+};
 
-function setGame() {
+const setGame = () => {
   fillBoard(solution);
   board = JSON.parse(JSON.stringify(solution));
-  removeDigits(board, difficulty); //remove the cells depemds on difficulty
+  removeDigits(board, difficulty); //remove the cells depends on difficulty
 
-  document.getElementById("digits").innerHTML = "";
-  document.getElementById("board").innerHTML = "";
+  document.getElementById("digits").innerHTML = ""; //initialize the digits
+  document.getElementById("board").innerHTML = ""; // initialize the board
 
   //make digit
   for (let i = 1; i <= 9; i++) {
     let number = document.createElement("div");
     number.id = i;
     number.innerText = i;
-    number.addEventListener("click", selectNumber);
+    number.addEventListener("click", selectNumber); //when click, call selectNumber()
     number.classList.add("number");
     document.getElementById("digits").appendChild(number);
   }
@@ -135,30 +124,31 @@ function setGame() {
       let tile = document.createElement("div");
       tile.id = r.toString() + "-" + c.toString();
       if (board[r][c] != "-") {
+        //check if the cell is empty
         tile.innerText = board[r][c];
         tile.classList.add("tile-start");
       }
       if (r == 2 || r == 5) {
-        tile.classList.add("horizontal-line");
+        tile.classList.add("horizontal-line"); //make the line thick
       }
       if (c == 2 || c == 5) {
-        tile.classList.add("vertical-line");
+        tile.classList.add("vertical-line"); //make the line thick
       }
-      tile.addEventListener("click", selectTile);
+      tile.addEventListener("click", selectTile); //when the cell is clicked, call selectTile()
       tile.classList.add("tile");
       document.getElementById("board").append(tile);
     }
   }
-
+  //initialize error and timer
   errors = 0;
   document.getElementById("errors").innerText = errors;
   timeRemaining = 15 * 60;
   document.getElementById("timer").innerText = "15:00";
-  timerStarted = false;
+  timerStarted = false; //not start
   clearInterval(timerInterval);
-}
+};
 
-function disableBoard() {
+const disableBoard = () => {
   let tiles = document.querySelectorAll(".tile");
   tiles.forEach((tile) => {
     tile.removeEventListener("click", selectTile);
@@ -167,16 +157,16 @@ function disableBoard() {
   numbers.forEach((number) => {
     number.removeEventListener("click", selectNumber);
   });
-}
+};
 
-function startTimer() {
+const startTimer = () => {
   if (!timerStarted) {
     timerStarted = true;
     timerInterval = setInterval(updateTimer, 1000);
   }
-}
+};
 
-function updateTimer() {
+const updateTimer = () => {
   if (timeRemaining <= 0) {
     clearInterval(timerInterval);
     alert("Time's up!");
@@ -189,43 +179,43 @@ function updateTimer() {
   document.getElementById("timer").innerText = `${minutes}:${
     seconds < 10 ? "0" : "" //insert zero before seconds ex 01 02
   }${seconds}`;
-}
+};
 
-function gameOver() {
+const gameOver = () => {
   clearInterval(timerInterval);
   alert("Game Over! Too many errors.");
   disableBoard();
-}
+};
 
-function selectNumber() {
+const selectNumber = (event) => {
   if (numSelected != null) {
-    //when select number
-    numSelected.classList.remove("number-selected"); //if the cell is already chosen, initialize (remove information)
-  }
-  numSelected = this; //the number clicked just now
+    //if the number already existed
+    numSelected.classList.remove("number-selected");
+  } //remove the class
+  numSelected = event.target;
   numSelected.classList.add("number-selected");
-  startTimer(); // Start the timer when a number is selected
-}
+  startTimer();
+};
 
-function selectTile() {
-  if (numSelected) {
-    if (this.innerText != "") {
-      //the cell has already number
-      return; //finish
+const selectTile = (event) => {
+  if (numSelected != null) {
+    if (event.target.innerText != "") {
+      //if the cellhas already number
+      return; //finish the function
     }
-
-    let coords = this.id.split("-"); //0-0, 1-0, 2-3 0,0 1,0 2,3
-    let r = parseInt(coords[0]); //first value
-    let c = parseInt(coords[1]); //second value
+    let coords = event.target.id.split("-");
+    let r = parseInt(coords[0]);
+    let c = parseInt(coords[1]);
     if (solution[r][c] == numSelected.id) {
-      this.innerText = numSelected.id; //we can put the number
-      board[r][c] = numSelected.id; //update the board
+      event.target.innerText = numSelected.id;
+      board[r][c] = numSelected.id;
       if (checkWin()) {
         clearInterval(timerInterval);
         alert("Congratulations!!");
         disableBoard();
       }
     } else {
+      //if failed
       errors += 1;
       document.getElementById("errors").innerText = errors;
       if (errors >= maxErrors) {
@@ -234,9 +224,16 @@ function selectTile() {
     }
     startTimer();
   }
-}
+};
 
-function newGame() {
+document.querySelectorAll(".number").forEach((number) => {
+  number.addEventListener("click", selectNumber);
+});
+document.querySelectorAll(".tile").forEach((tile) => {
+  tile.addEventListener("click", selectTile);
+});
+
+const newGame = () => {
   const newArr = [
     ["-", "-", "-", "-", "-", "-", "-", "-", "-"],
     ["-", "-", "-", "-", "-", "-", "-", "-", "-"],
@@ -251,14 +248,15 @@ function newGame() {
   board = newArr;
   solution = newArr;
   setGame();
-}
+};
 
-function setDifficulty(numToRemove) {
+//change the level
+const setDifficulty = (numToRemove) => {
   difficulty = numToRemove;
   newGame();
-}
+};
 
-setGame(); //initialize game
+setGame(); //reset game
 document.getElementById("new-game-button").addEventListener("click", newGame);
 document
   .getElementById("easy")
@@ -268,4 +266,4 @@ document
   .addEventListener("click", () => setDifficulty(50));
 document
   .getElementById("hard")
-  .addEventListener("click", () => setDifficulty(55));
+  .addEventListener("click", () => setDifficulty(60));
